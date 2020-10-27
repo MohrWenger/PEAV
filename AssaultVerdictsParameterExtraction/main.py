312a9ba2faeb59e5e459ee14206240510fbb566f
tomka <tom.kalir@mail.huji.ac.il>
2020-10-27 15:03:57 +0200

    basic structure

---
+++ AssaultVerdictsParameterExtraction/main.py
@@ -0,0 +1,26 @@
+
+
+def nameExtract(file):
+    pass
+
+
+def ExtractParameters(file, db):
+    # think of a good structure to call each function of extraction and put the output in the correct column
+    pass
+
+
+def createNewDB():
+    # create a xls file with the right columns as the parameters
+    # TODO: idea to use a dictionary as the structure to create this DB
+    pass
+
+
+"""
+This function receives a path with many verdicts (presumably in word or html format), and uses the code to create a
+database
+"""
+def fromVerdictsToDB(path):
+    # call createNewDB
+    # go through files in a loop
+        # for each file, call ExtractParameters
+    pass
