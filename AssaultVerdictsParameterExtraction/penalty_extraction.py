5a83210f2222122e7f1ecf0fdfebf08bb333ddfc
mohrWenger <mohr.wenger669@gmail.com>
2021-04-01 11:18:01 +0300

    pessach

--- AssaultVerdictsParameterExtraction/penalty_extraction.py
+++ AssaultVerdictsParameterExtraction/penalty_extraction.py
@@ -38,8 +38,8 @@ PROBATION = 1
 COM_SERVICE = 2
 
 YEAR_REG = r"([0-3]{0,1}[0-9]\/[0-2]{0,1}[0-9]\/[0-2]{0,1}[0-9]{1,3})|([0-3]{0,1}[0-9]\.[0-2]{0,1}[0-9]\.[0-2]{0,1}[0-9]{1,3})"
-HEB_YEAR_EXP = "שנ(ים)*(ות)*|שנה|שנתיים"
-HEB_MONTH_EXP = "ח(ו)*דש"
+HEB_YEAR_EXP = "שנ[ים]*[ות]*|שנה|שנתיים"
+HEB_MONTH_EXP = "חו*דש"
 YEARS = "שנים"
 MONTHS = "חודשים"
 
@@ -57,22 +57,96 @@ NUM_UNITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
          "אחד", "שתים", "שתיים", "שלוש", "ארבע", "חמש", "שש", "שבע", "שמונה", "תשע",
          "עשר", "אחת", "שנים", "שניים", "", "חמישה", "", "שישה"]
 
-def vote_for_time(t, act_sent):
+regex_numbers_heb = ["(אח[ד|ת] |חודש )", "(שנתיי?ם|חודשיים|שתיי?ם|שניים)", "(שלושה?)", "(ארבעה?)", "(חמי?שה?)",
+                     "(\sשישה|שש)", "(שי?בעה?)", "(שמונה?)", "(תשעה?)", "(עשרה?)"]
+ten = "(\s?-?\s?עשרה?)?"
+and_str = "(ו?)"
+tens = "(ים)?"
+
+
+def numberExchange(sentence):
     """
-    This function recieves a number and a sentence and votes about the likelyhood of it being the actual jail time.
+    This function receives a sentence and replaces the word phrases for number with integers
+    :param sentence:
+    :return: A sentence after the replacement was done
+    """
+    i = 1
+    new_sentence = sentence
+    for word in regex_numbers_heb:  # Go over the regexes and find number expressions
+        pattern = and_str + word + tens + ten  # Creating a regex with groups according to hebrew language rules
+        heb_numbers = [m.span() for m in re.finditer(pattern, sentence)]  # A list of full matches and not groups
+        print(pattern)
+        print(heb_numbers)
+        for tup in heb_numbers:
+            value = 0
+            match = re.match(pattern, sentence[tup[0]:tup[1]])  # This is meant to find the groups within a previuosly found pattern
+
+            if match.group(3):  # Finding if there is an hebrew suffix for tens (if found multiply).
+                if word == regex_numbers_heb[-1]:
+                    value = 20
+                else:
+                    value = i * 10
+                next = sentence[tup[1] + 1:].find(" ")
+                for j in range(len(regex_numbers_heb)):  # Finding if there is a ones value and if so what is it.
+                    pattern2 = and_str + regex_numbers_heb[j] + tens + ten
+                    match2 = re.match(pattern2, sentence[tup[1] + 1:tup[1] + next + 1])
+                    if match2 and match2.group(1):  # Checks whether there is indeed "and" before the digit.
+                        pattern += " " + pattern2
+                        value += j + 1
+                        break
+
+            if not match.group(3):  # Then it is a number up until 10 (no prefix or suffix)
+                value = i
+
+            if match.group(4):  # Between 10 and 20
+                value = i + 10
+            print(value)
+            new_sentence = re.sub(pattern, str(value), new_sentence)
+            print(new_sentence)
+        i += 1
+
+    return new_sentence
+
+def vote_for_time(t, act_sent, t_units):
+    """
+    This function receives a number and a sentence and votes about the likelihood of it being the actual jail time.
     :param t: a number
     :param act_sent: the sentence which this number was taken from.
     :return: a score for this sentence
     """
+    if act_sent.find((BAFOAL)) != -1:
+        score = np.abs(act_sent.find(BAFOAL) - act_sent.find(t)) #starting from measuring the distance to the word of actual.
+    else:
+        score = 0
+
     time_ind = act_sent.find(t)
-    score = np.abs(act_sent.find(BAFOAL) - time_ind ) #Distance from bafoal
-    ext = act_sent.find("יתר")
-    page = act_sent.find("עמוד")
-    if ext != -1:
-        if time_ind < ext:
-            score -= 10
-    if np.abs (time_ind - page) < 10:
+    # Make sure it is not a page index.
+    if act_sent.find("עמוד") != -1 and act_sent.find("עמוד") <= act_sent.find(t) <= (act_sent.find("עמוד") + 6):
         score += 100
+
+    #Distance from a time unit
+    units = [(-1,-1)]
+    if t_units == MONTH:
+        units = re.findall(HEB_MONTH_EXP, act_sent)
+    elif t_units == YEAR:
+        units = re.findall(HEB_YEAR_EXP, act_sent)
+
+    for u in units: #start from time_ind
+        if type(u[1]) == str:
+            ind = act_sent[time_ind:].find(u)
+            if ind != -1 and np.abs(ind - time_ind) < 5:
+                score -= 1000
+                break
+
+    #Words that it is less likely to appear after
+    start_time = act_sent.find("יום")
+    if start_time != -1 and time_ind < start_time:
+        score += 10
+
+    if len(act_sent) - time_ind < 4:
+        score += 50
+
+    return score
 
 def find_time_act(act_sent):
     """
@@ -80,28 +154,35 @@ def find_time_act(act_sent):
     :param act_sent:
     :return:
     """
+    act_sent = numberExchange(act_sent)
+    # act_sent = replace_value_with_key(act_sent)
     times = re.findall("[0-9]+", act_sent) #finds all the numbers, assumes word numbers were converted.
-    times_and_dist = {}
-    min_dist = 10000
+    times_and_score = {}
+    min_score = 10000
     winner_time = '0'
 
-    if len(times) > 2 and int(times[0]) == (int(times[1]) + int(times[2])):
+    if len(times) > 2 and int(times[0]) == (int(times[1]) + int(times[2])): #A structure that returns: X1 time in total X2 actual and X3 probation
+        winner_time = times[1]
+
+    elif act_sent.find("יתר") != -1:
         winner_time = times[1]
 
     else:
+        t_units = find_time_units(act_sent)
         for t in times:
-            if act_sent.find("עמוד") != -1 and act_sent.find("עמוד") <= act_sent.find(t) <= (act_sent.find("עמוד") + 6):
+            if (t_units == YEAR and int (t) > 30) or (t_units == MONTH and int(t) > 30 *12) :
+                print("entered this")
                 continue
 
-            vote_for_time(t, act_sent)
-            dist = np.abs(act_sent.find(BAFOAL) - act_sent.find(t))
-            times_and_dist[t] = dist
-            if dist < min_dist:
-                min_dist = dist
-                winner_time = t
+            else:
+                score = vote_for_time(t, act_sent,t_units)
+                times_and_score[t] = score
+                if score < min_score:
+                    min_score = score
+                    winner_time = t
 
             # reg = '\b(?:'+t+'\W+(?:\w+\W+){0,'+str(dist)+'}?'+BAFOAL+'|'+BAFOAL+'\W+(?:\w+\W+){0,'+str(dist)+'}?'+t+')\b'
-    return times_and_dist, winner_time
+    return times_and_score, winner_time
 
 def replace_value_with_key(sentence):
     """
@@ -215,12 +296,13 @@ def extracting_penalty(text, filename ):
                 max_score_prob = scr_prob
                 main_sentence_prob = sentence
 
-        all_times, time, time_unit = extract_time_from_sentence(main_sentence_act)
-
+        all_times, prison_time, time_unit = extract_time_from_sentence(main_sentence_act)
+    main_sentence_prob = numberExchange(main_sentence_act)
     return penalty, main_sentence_act, main_sentence_prob, all_times, prison_time,time_unit #כל מה שהפונקציה מחזירה שיהיה אח כך ב DB
 
 def extract_time_from_sentence(sentence):
-        main_sentence_act = replace_value_with_key(sentence) #TODO notice This is turned off for sentence validation purposes
+        # main_sentence_act = replace_value_with_key(sentence) #TODO notice This is turned off for sentence validation purposes
+        main_sentence_act = sentence
         all_times, prison_time = find_time_act (main_sentence_act)
         time_unit = find_time_units(main_sentence_act)
 
@@ -245,7 +327,7 @@ def fromVerdictsToDB():
     :param df:
     :return:
     """
-    batch = pd.DataFrame()
+    db = pd.DataFrame()
     directory = VERDICTS_DIR               #text files eddition:
     counter = 0
 
@@ -257,19 +339,19 @@ def fromVerdictsToDB():
             #     if type(files[i]) == str:
             #         filename = add_to_txt_db(files[i], urlToText(files[i]), "mechozi")
 
-            if filename.endswith(".txt") and filename in relevant_cases:
+            if filename.endswith(".txt") and counter < 150:# and filename in relevant_cases:
                 counter += 1
                 file_name = os.path.join(directory, filename)
                 text = open(file_name, "r", encoding="utf-8").read()
 
                 print("^^^ File is ", file_name, " ^^^ - not psak"," counter = " ,counter)
-                if filename.find("00001581-359.txt") != -1:
+                if filename.find("ME-09-127-11.txt") != -1:
                     print("break point")
 
                 main_penalty, sentence, all_sentences, all_times, time, time_unit = extracting_penalty(text, filename) #Here I call the penalty func
 
-                batch.loc[i,"PENALTY_SENTENCE"] = main_penalty
-                batch.loc[i,"VOTED TIME"] = time
+                # batch.loc[i,"PENALTY_SENTENCE"] = main_penalty
+                # batch.loc[i,"VOTED TIME"] = time
 
                 sentence_line = pd.DataFrame([[filename,"Gzar", main_penalty,     sentence,          all_sentences,   all_times,       time, time_unit]], #here I add values to DB
                                              columns =[CASE_NUM,     "TYPE","Main Punishment","PENALTY_SENTENCE", "ALL SENTENCES", "OPTIONAL TIMES", "VOTED TIME", "units"]) #Here adding a title
@@ -280,11 +362,40 @@ def fromVerdictsToDB():
 
             db.to_csv('verdict_penalty.csv', encoding= 'utf-8')
 
-def from_sentence_list(sentence_list):
-    pass
+def from_sentence_list(case_names, sentence_list):
+    """
+    This function is used for main penalty time from a list of sentences.
+    :param case_names: A list of the cases names
+    :param sentence_list: A list of the sentences corresponding to the list of sentences
+    :return: Nothing, Exports a CSV file with the sentences and extracted times.
+    """
+    s_db = pd.DataFrame()
+    for i in range(len(sentence_list)):
+
+        print('i = ', i)
+        if i == 34:
+            print("b point")
+        if type(sentence_list[i]) == str:
+            all_times, prison_time, time_unit = extract_time_from_sentence(sentence_list[i])
+            sentence_line = pd.DataFrame(
+                [[case_names[i], sentence_list[i],all_times, prison_time, time_unit]],
+                # here I add values to DB
+                columns=[CASE_NUM,"PENALTY_SENTENCE","OPTIONAL TIMES","VOTED TIME", "units"])  # Here adding a title
+            s_db = pd.concat([s_db, sentence_line])
+        else:
+            sentence_line = pd.DataFrame(
+                [[case_names[i], sentence_list[i], " -- ", " -- ", " -- "]],
+                # here I add values to DB
+                columns=[CASE_NUM, "PENALTY_SENTENCE", "OPTIONAL TIMES", "VOTED TIME", "units"])  # Here adding a title
+            s_db = pd.concat([s_db, sentence_line])
+
+    s_db.to_csv('pipline on test set.csv', encoding='utf-8')
+
+def pipline_on_test_set():
+    test_set = pd.read_csv("Test Set - PEAV - Sheet1.csv", error_bad_lines=False)
+    from_sentence_list(test_set["Case_Name"], test_set["עונש בפועל (המשפט הרלוונטי מהטקסט)"])
 
 if __name__ == "__main__":
-
     # district_dict = {}
     with open('data.txt') as json_file:
         district_dict = json.load(json_file)
@@ -300,3 +411,5 @@ if __name__ == "__main__":
         verdicts_list = json.load(json_file)
 
     fromVerdictsToDB()
+
+    # pipline_on_test_set()
