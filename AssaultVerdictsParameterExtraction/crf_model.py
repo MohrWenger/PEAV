955644a40a088723eb4e721c5f022e1644f2c115
mohrWenger <mohr.wenger669@gmail.com>
2021-05-23 17:38:34 +0300

    A bunch of stuff we copied ... pushing to save

--- AssaultVerdictsParameterExtraction/crf_model.py
+++ AssaultVerdictsParameterExtraction/crf_model.py
@@ -1,197 +1,125 @@
 import nltk
 import sklearn_crfsuite
-import eli5
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-nltk.corpus.conll2002.fileids()
-train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
-test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
-print(train_sents[0])
 
-def word2features(sent, i):
-    word = sent[i][0]
-    postag = sent[i][1]
+TAG_COL = "does sentence include punishment"
+TAG_PROB = 'probation'
+SENTENCE = "sentence"
+FILE_NAME = "filename"
 
-    features = {
-        'bias': 1.0,
-        'word.lower()': word.lower(),
-        'word[-3:]': word[-3:],
-        'word.isupper()': word.isupper(),
-        'word.istitle()': word.istitle(),
-        'word.isdigit()': word.isdigit(),
-        'postag': postag,
-        'postag[:2]': postag[:2],
-    }
-    if i > 0:
-        word1 = sent[i-1][0]
-        postag1 = sent[i-1][1]
-        features.update({
-            '-1:word.lower()': word1.lower(),
-            '-1:word.istitle()': word1.istitle(),
-            '-1:word.isupper()': word1.isupper(),
-            '-1:postag': postag1,
-            '-1:postag[:2]': postag1[:2],
-        })
+
+def create_as_dict(df):
+    as_dict = []
+    for k, row in df.iterrows():
+        as_dict.append([dict(**row)])
+    return as_dict
+
+
+def remove_strings (db):
+    # db = db.replace(db[SENTENCE].tolist(), db.index.tolist())  # This is the line that replaces sentences
+    # db.insert(0,"new col", db.index.tolist() )                                                              # with indx.
+    db = db.set_index(pd.Index(np.arange(len(db[SENTENCE]))))                                                              # with indx.
+    le = LabelEncoder()
+    file_name_dict = le.fit(db[FILE_NAME])
+    db[FILE_NAME] = file_name_dict.fit_transform(db[FILE_NAME])
+
+    return db
+
+def add_line(new_db, line, tag_name, prediction, db):
+        sentence = db.sentence[line]
+        # print(sentence)
+        # line = db.loc[db[SENTENCE] == sentence]
+        line = db.iloc[line]
+        if prediction == 1:
+            add = pd.DataFrame(
+                [[line[FILE_NAME], sentence, line[tag_name], prediction]],
+                columns=["file", "sentence", "original tag", "our tag"])
+            new_db = pd.concat([new_db, add])
+        return new_db
+
+
+def using_crfsuite(x_db, tag, tag_name, df, test = True):
+    x_db = remove_strings(x_db)
+
+    np.random.seed(42)
+    temp_db = x_db.loc[:, x_db.columns != SENTENCE]
+    x_train, x_test, y_train, y_test = train_test_split(temp_db, tag, shuffle=True, test_size=0.2, random_state=42)
+    x_train_dict = create_as_dict(x_train)
+    y_train = [[str(x)] for x in y_train]
+
+    crf = sklearn_crfsuite.CRF(
+        algorithm='lbfgs',
+        c1=0.1,
+        c2=0.1,
+        max_iterations=100,
+        all_possible_transitions=True
+    )
+    print(len(x_train_dict))
+    print(len(y_train))
+    crf.fit(x_train_dict, y_train)
+
+    if test:
+        x = x_test
+        y = y_test
     else:
-        features['BOS'] = True
+        x = x_train
+        y = y_train
 
-    if i < len(sent)-1:
-        word1 = sent[i+1][0]
-        postag1 = sent[i+1][1]
-        features.update({
-            '+1:word.lower()': word1.lower(),
-            '+1:word.istitle()': word1.istitle(),
-            '+1:word.isupper()': word1.isupper(),
-            '+1:postag': postag1,
-            '+1:postag[:2]': postag1[:2],
-        })
-    else:
-        features['EOS'] = True
+    # create predictions of which are the correct sentences
+    predicted_results = crf.predict(x)
+    goal_labels = y.to_numpy()
+    original_indices = x.index.to_numpy()
+    check_prediction(predicted_results, goal_labels, tag_name,original_indices, df)
 
-    return features
+def check_prediction(predicted_results, goal_labels,tag_name ,X, df):
+    count_same = 0
+    true_positive = 0
+    true_negative = 0
+    false_negative = 0
+    false_positive = 0
+    ones = pd.DataFrame()
+    for i in range(len(predicted_results)):
+        if predicted_results[i] == goal_labels[i]:
+            count_same += 1
+            if predicted_results[i] == 1:
+                true_positive += 1
+                ones = add_line(ones, X[i], tag_name, predicted_results[i], df)
 
+                # print(X[i])
+                # print(df.sentence[X[i]])
+                # print(df[tag_name][X[i]], "and goals = ", goal_labels[i])
 
-def sent2features(sent):
-    return [word2features(sent, i) for i in range(len(sent))]
+            elif predicted_results[i] == 0:
+                true_negative += 1
 
-def sent2labels(sent):
-    return [label for token, postag, label in sent]
+        elif goal_labels[i] == 1:
+                false_negative +=1
+        else:
+            false_positive += 1
+            ones = add_line(ones, X[i], tag_name, predicted_results[i], df)
 
-def sent2tokens(sent):
-    return [token for token, postag, label in sent]
+            # print(df.sentence[X[i]])
+    print("sample size: ", len(goal_labels))
+    print("how many ones expected:", sum(goal_labels))
+    print("how many ones predicted: ", sum(predicted_results))
+    print("Precision: ", (true_positive)/(true_positive + false_positive))
+    print("Recall: ", true_positive/sum(goal_labels))
+    print("F1 score = ", calc_F1(true_positive, true_negative, false_negative*weights, false_positive))
 
-X_train = [sent2features(s) for s in train_sents]
-y_train = [sent2labels(s) for s in train_sents]
+def calc_F1 (true_positive, true_negative, false_negative, false_positive):
+    return true_positive/(true_positive+0.5*(false_positive+false_negative))
 
-X_test = [sent2features(s) for s in test_sents]
-y_test = [sent2labels(s) for s in test_sents]
-
-print(sent2features(train_sents[0])[0])
-print(X_train[0][1])
-
-# import torch
-#
-#
-# def neg_log_likelihood(self, rolls, states):
-#     """Compute neg log-likelihood for a given sequence.
-#
-#     Input:
-#         rolls: numpy array, dim [1, n_rolls]. Integer 0-5 showing value on dice.
-#         states: numpy array, dim [1, n_rolls]. Integer 0, 1. 0 if dice is fair.
-#     """
-#     loglikelihoods = self._data_to_likelihood(rolls)
-#     states = torch.LongTensor(states)
-#
-#     sequence_loglik = self._compute_likelihood_numerator(loglikelihoods, states)
-#     denominator = self._compute_likelihood_denominator(loglikelihoods)
-#     return denominator - sequence_loglik
-#
-#
-# def _data_to_likelihood(self, rolls):
-#     """Converts a numpy array of rolls (integers) to log-likelihood.
-#     self.loglikeligood is a matrix of 6 x 2 in our case.
-#     Input is one [1, n_rolls]
-#     """
-#     log_likelihoods = self.loglikelihood[rolls]
-#     return Variable(torch.FloatTensor(log_likelihoods), requires_grad=False)
-#
-#
-# def _compute_likelihood_numerator(self, loglikelihoods, states):
-#     """Computes numerator of likelihood function for a given sequence.
-#
-#     We'll iterate over the sequence of states and compute the sum
-#     of the relevant transition cost with the log likelihood of the observed
-#     roll.
-#     Input:
-#         loglikelihoods: torch Variable. Matrix of n_obs x n_states.
-#                         i,j entry is loglikelihood of observing roll i given state j
-#         states: sequence of labels
-#     Output:
-#         score: torch Variable. Score of assignment.
-#     """
-#     prev_state = self.n_states
-#     score = Variable(torch.Tensor([0]))
-#     for index, state in enumerate(states):
-#         score += self.transition[state, prev_state] + loglikelihoods[index, state]
-#         prev_state = state
-#     return score
-#
-#
-# def _compute_likelihood_denominator(self, loglikelihoods):
-#     """Implements the forward pass of the forward-backward algorithm.
-#
-#     We loop over all possible states efficiently using the recursive
-#     relationship: alpha_t(j) = \sum_i alpha_{t-1}(i) * L(x_t | y_t) * C(y_t | y{t-1} = i)
-#     Input:
-#         loglikelihoods: torch Variable. Same input as _compute_likelihood_numerator.
-#                         This algorithm efficiently loops over all possible state sequences
-#                         so no other imput is needed.
-#     Output:
-#         torch Variable.
-#     """
-#
-#     # Stores the current value of alpha at timestep t
-#     prev_alpha = self.transition[:, self.n_states] + loglikelihoods[0].view(1, -1)
-#
-#     for roll in loglikelihoods[1:]:
-#         alpha_t = []
-#
-#         # Loop over all possible states
-#         for next_state in range(self.n_states):
-#
-#             # Compute all possible costs of transitioning to next_state
-#             feature_function = self.transition[next_state,:self.n_states].view(1, -1) + \
-#                                roll[next_state].view(1, -1).expand(1, self.n_states)
-#
-#             alpha_t_next_state = prev_alpha + feature_function
-#             alpha_t.append(self.log_sum_exp(alpha_t_next_state))
-#         prev_alpha = torch.cat(alpha_t).view(1, -1)
-#     return self.log_sum_exp(prev_alpha)
-#
-#
-# def _viterbi_algorithm(self, loglikelihoods):
-#     """Implements Viterbi algorithm for finding most likely sequence of labels.
-#
-#     Very similar to _compute_likelihood_denominator but now we take the maximum
-#     over the previous states as opposed to the sum.
-#     Input:
-#         loglikelihoods: torch Variable. Same input as _compute_likelihood_denominator.
-#     Output:
-#         tuple. First entry is the most likely sequence of labels. Second is
-#                the loglikelihood of this sequence.
-#     """
-#
-#     argmaxes = []
-#
-#     # prev_delta will store the current score of the sequence for each state
-#     prev_delta = self.transition[:, self.n_states].view(1, -1) + \
-#                  loglikelihoods[0].view(1, -1)
-#
-#     for roll in loglikelihoods[1:]:
-#         local_argmaxes = []
-#         next_delta = []
-#         for next_state in range(self.n_states):
-#             feature_function = self.transition[next_state,:self.n_states].view(1, -1) + \
-#                                roll.view(1, -1) + \
-#                                prev_delta
-#             most_likely_state = self.argmax(feature_function)
-#             score = feature_function[0][most_likely_state]
-#             next_delta.append(score)
-#             local_argmaxes.append(most_likely_state)
-#         prev_delta = torch.cat(next_delta).view(1, -1)
-#         argmaxes.append(local_argmaxes)
-#
-#     final_state = self.argmax(prev_delta)
-#     final_score = prev_delta[0][final_state]
-#     path_list = [final_state]
-#
-#     # Backtrack through the argmaxes to find most likely state
-#     for states in reversed(argmaxes):
-#         final_state = states[final_state]
-#         path_list.append(final_state)
-#
-#     return np.array(path_list), final_score
-#
-#
-#
-# if __name__ == "__main__":
-#     _viterbi_algorithm()
+if __name__ == "__main__":
+    path = r"D:\PEAV\AssaultVerdictsParameterExtraction\DB of 16.5 - Sheet1.csv"
+    # path = "/Users/tomkalir/Projects/PEAV/AssaultVerdictsParameterExtraction/feature_DB - feature_DB (1).csv"
+    # path = r"C:\Users\נועה וונגר\PycharmProjects\PEAV\AssaultVerdictsParameterExtraction\feature_DB - feature_DB (1).csv"
+    db_initial = pd.read_csv(path, header=0, na_values='')
+    db_filtered = db_initial
+    x_db = db_filtered.loc[:, db_filtered.columns != TAG_COL]
+    x_db = x_db.loc[:, x_db.columns != TAG_COL]
+    tag = db_filtered[TAG_COL]
+    using_crfsuite(x_db, tag, TAG_COL, db_filtered)
